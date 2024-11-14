b6622f21be35187d35e161f3add28df50db61517
edouard <edouardf@apadmi.com>
2024-11-14 14:19:55 +0000

    feat: TZ1 Attempt to write a Header for DNS request

---
+++ app/dnsHeader.ts
@@ -0,0 +1,62 @@
+export class DnsHeader {
+	private id: number;
+	private queryResponse: number;
+	private opcode: number;
+	private aa: number;
+	private truncation: number;
+	private recursionDesired: number;
+	private recursionAvailable: number;
+	private reserved: number;
+	private responseCode: number;
+	private questionCount: number;
+	private answerRecordCount: number;
+	private authorityRecordCount: number;
+	private additionalRecordCount: number;
+
+	public encode(): Buffer {
+		const header: Buffer = Buffer.alloc(12);
+
+		// ID
+		this.id = 1234;
+		this.queryResponse = 1;
+		this.opcode = 0;
+		this.aa = 0;
+		this.truncation = 0;
+		this.recursionDesired = 0;
+		this.recursionAvailable = 0;
+		this.reserved = 0;
+		this.responseCode = 0;
+		this.questionCount = 0;
+		this.answerRecordCount = 0;
+		this.authorityRecordCount = 0;
+		this.additionalRecordCount = 0;
+
+		// write the id
+		header.writeUint16BE(this.id, 0);
+
+
+		// create byte from flag
+		let byte = 0;
+		byte |= this.queryResponse << 15;
+		byte |= this.opcode << 11;
+		byte |= this.aa << 10;
+		byte |= this.truncation << 9;
+		byte |= this.recursionDesired << 8;
+		byte |= this.recursionAvailable << 7;
+		byte |= this.reserved << 3;
+		byte |= this.responseCode << 0;
+
+		console.log("BYTE Data: " + byte);
+		header.writeUint8(byte, 2);
+
+		header.writeUint16BE(this.questionCount, 4);
+
+		header.writeUint16BE(this.answerRecordCount, 6);
+
+		header.writeUint16BE(this.authorityRecordCount, 8);
+
+		header.writeUint16BE(this.additionalRecordCount, 10);
+
+		return header;
+	}
+}
--- app/main.ts
+++ app/main.ts
@@ -1,16 +1,25 @@
 import * as dgram from "dgram";
-// You can use print statements as follows for debugging, they'll be visible when running tests.
+import { Buffer } from "node:buffer";
+import { DnsHeader } from "./dnsHeader";
 
 const udpSocket: dgram.Socket = dgram.createSocket("udp4");
 
 udpSocket.bind(2053, "127.0.0.1");
 
+
+if (udpSocket) {
+	console.log("Server created");
+}
+
 udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
+	console.log(data.toString());
 	try {
 		console.log(
 			`Received data from ${remoteAddr.address}:${remoteAddr.port}`,
 		);
-		const response = Buffer.from("");
+		const header = new DnsHeader();
+		const response = new Uint8Array(header.encode());
+		console.log(response);
 		udpSocket.send(response, remoteAddr.port, remoteAddr.address);
 	} catch (e) {
 		console.log(`Error sending data: ${e}`);
